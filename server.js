b5bd5117a72be8ba51f74033923a1a180c54e24c
roselaird <rosemlaird@gmail.com>
2024-01-17 12:53:26 +0000

    created clothes list, now returns clothes

--- server.js
+++ server.js
@@ -2,14 +2,22 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = 3000; // You can choose any available port
+const fs = require('fs');
+
+const clothesFile = 'clothes/clothes.json';
+
+app.use(express.static('client'));
+app.use(express.json());
+
+const clothesList = JSON.parse(fs.readFileSync(clothesFile));
 
 app.get('/', (req, res) => {
     res.sendFile('index.html', { root: __dirname });
 });
 
-app.get('/c', (req, res) => {
-  //res.send('This is the /clothes route!');
-  res.sendFile('top_1.jpg', { root: __dirname });
+app.get('/clothes', (req, res) => {
+  //res.sendFile('top_1.jpg', { root: __dirname });
+  res.send(clothesList[0].name)
 });
 
 app.listen(port, () => {
